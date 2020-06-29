37dab0695a952f036f2aa3c0bcd3b6562e865ed5
cl1688 <1245010531@qq.com>
2020-06-29 10:07:06 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,13 @@ class UsersController extends Controller
     
     
     public function store(UserRequest $request){
-    	return [];
+    	$user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+    	session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+    	return redirect()->route('users.show', [$user]);
+    	
     }
 }
